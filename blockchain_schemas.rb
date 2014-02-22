8cd83a752ca7a9705f5ccf74405e4e703bb9dd07
benzrf <benzrf@benzrf.com>
2014-02-21 21:49:58 -0500

    Cool.

--- blockchain_schemas.rb
+++ blockchain_schemas.rb
@@ -1,13 +1,15 @@
 require_relative 'schema'
 
 module BlockchainSchemas
-	SHA1 = schema {all_of(String, methods(length: 40))}
+	str = proc {|len| schema {all_of(String, methods(length: len))}}
+	hex = proc {|s| s.chars.all? {|char| char =~ /[0-9a-f]/}}
+	SHA1 = schema {all_of(str[40], hex)}
 	Input = schema {tuple(SHA1, Fixnum)}
 	Output = schema {tuple(SHA1, Rational)}
 	Signature = schema {
 		all_of(
 			Signature,
-			methods(signed_hash: String, pubkey: String)
+			methods(signed_hash: str[256], pubkey: str[451])
 		)
 	}
 	Transaction = schema {
